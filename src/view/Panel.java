228851b2684b93e33aae7f72fa8eaae33c1c052d
Jac0bAnderson <jake912235@gmail.com>
2016-04-20 09:34:15 -0600

    Added a toggle button.

--- src/view/Panel.java
+++ src/view/Panel.java
@@ -10,6 +10,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
+import javax.swing.JToggleButton;
 import javax.swing.SpringLayout;
 
 import controller.Controller;
@@ -24,6 +25,8 @@ public class Panel extends JPanel
 	private JScrollPane arrayScroll;
 	private JButton sortButton;
 	private JTextArea textArea;
+	private boolean flip;
+	private JToggleButton testB;
 	public Panel(Controller baseController)
 	{
 	this.baseController = baseController;
@@ -32,8 +35,10 @@ public class Panel extends JPanel
 	textArea = new JTextArea("");
 	baseLayout.putConstraint(SpringLayout.NORTH, textArea, 71, SpringLayout.NORTH, this);
 	baseLayout.putConstraint(SpringLayout.WEST, textArea, 151, SpringLayout.WEST, this);
-	
-	
+	testB = new JToggleButton("sort");
+	baseLayout.putConstraint(SpringLayout.SOUTH, testB, -83, SpringLayout.SOUTH, this);
+	baseLayout.putConstraint(SpringLayout.EAST, testB, -162, SpringLayout.EAST, this);
+	flip = false;
 	//String [] nameCol = {"Show Name", "Genre", "Seasons"};
 	//Object [] [] info ={
 		//	{"Breaking Bad", "Crime Film", "5"},
@@ -59,7 +64,8 @@ public class Panel extends JPanel
 
 	private void setupPanel()
 	{
-		this.add(sortButton);
+		this.add(testB);
+		//this.add(sortButton);
 		this.add(textArea);
 		//this.add(arrayScroll);
 		this.setLayout(baseLayout);
@@ -80,15 +86,52 @@ public class Panel extends JPanel
 		sortButton.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent click)
-			{
-				baseController.insertionSort();
-				String temp ="the sorted things are: \n";
-				for(IronMan current : baseController.getIronDude())
+			{ 
+				
+					if(flip = false)
+					{
+						baseController.insertionSort();
+						String temp ="the sorted things are: \n";
+						for(IronMan current : baseController.getIronDude())
+						{
+							temp += current.toString() + "\n";
+						}
+						textArea.setText(temp);
+						//textArea.setText(baseController.showLevels());
+						flip = true;
+					}
+					else if(flip = true)
+					{
+						textArea.setText("");
+						flip = false;
+					}
+				
+			
+			}
+		});
+		testB.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{ 
+				if(testB.isSelected())
 				{
-					temp += current.toString() + "\n";
+					
+						baseController.insertionSort();
+						String temp ="the sorted things are: \n";
+						for(IronMan current : baseController.getIronDude())
+						{
+							temp += current.toString() + "\n";
+						}
+						textArea.setText(temp);
+						//textArea.setText(baseController.showLevels());
+						flip = true;
 				}
-				textArea.setText(temp);
-				//textArea.setText(baseController.showLevels());
+				if(! testB.isSelected())
+				{
+					textArea.setText("");
+				}
+					
+			
 			}
 		});
 	}
